4b678bee790fe39728e6a48b2504fc155a6544d6
Anas Baaj Blue10 <ab@blue10.com>
2026-05-24 16:48:20 +0200

    feat(pos): add public index.ts, remove old POSSaleScreen prototype

---
+++ src/features/pos/index.ts
@@ -0,0 +1,4 @@
+export { useSale } from './useSale'
+export { default as POSSaleScreen } from './POSSaleScreen.vue'
+export { default as SaleConfirmationScreen } from './SaleConfirmationScreen.vue'
+export type { Product } from './pos.types'
